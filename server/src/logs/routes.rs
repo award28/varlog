bc05bdb3d6105af5c207786f88387a9909ab0907
Austin Ward <ward.austin28@gmail.com>
2023-04-25 22:19:29 -0400

    Clean up logs and rev log reader

--- server/src/logs/routes.rs
+++ server/src/logs/routes.rs
@@ -3,7 +3,8 @@ use std::{fs, path::Path, io};
 use actix_web::{web, get, HttpResponse, Responder};
 use serde::Deserialize;
 
-use crate::auth::claims::Claims;
+use crate::{auth::claims::Claims, http::{HttpError, internal_err}};
+use super::rev_log_reader::RevLogReader;
 
 #[derive(Debug, Deserialize)]
 pub struct LogsRequest {
@@ -17,10 +18,15 @@ async fn logs(
     claims: web::ReqData<Claims>,
 ) -> impl Responder {
     let paths_res = visit_dirs(Path::new("/var/log"));
-    if paths_res.is_err() {
-        return crate::http::internal_err();
-    }
-    let files = paths_res.unwrap();
+    let files = {
+        match paths_res {
+            Ok(files) => files,
+            Err(e) => {
+                error!("Error walking /var/log dir: {e}");
+                return internal_err();
+            },
+        }
+    };
     let files = files
         .iter()
         .filter_map(|filename| {
@@ -29,7 +35,9 @@ async fn logs(
                 None => None,
             }
         })
-        .filter(|filename| claims.data.file_access_authorized(filename))
+        .filter(|filename| {
+            claims.data.file_access_authorized(filename)
+        })
         .collect::<Vec<_>>();
 
     HttpResponse::Ok().json(files)
@@ -60,38 +68,54 @@ async fn log(
     let (filename,) = path.into_inner();
 
     if filename.contains("..") {
-        return HttpResponse::Forbidden().json(
-            crate::http::HttpError {
-                error: String::from("The use of the `..` operator is forbidden."),
-            }
-        );
+        return HttpResponse::Forbidden()
+            .json(HttpError {
+                error: String::from("The `..` operator is forbidden."),
+            });
     }
 
-    // TODO: Make sure path exists and is a file.
-    match claims.data.file_access_authorized(&filename) {
-        true => {
-            let filename = if filename.starts_with("/var/log") {
-                filename.to_owned()
-            } else {
-                format!("/var/log/{filename}")
-            };
-
-            let pat = logs_req.pattern.to_owned().unwrap_or(String::new());
-            let lines = crate::logs::RevLogReader::new(filename)
-                .iter()
-                .filter(|line| line.contains(pat.as_str()))
-                .skip(logs_req.skip.unwrap_or(0))
-                .take(logs_req.take.unwrap_or(10))
-                .collect::<Vec<_>>();
-
-            HttpResponse::Ok().json(
-                lines,
-            )
-        },
-        false => HttpResponse::Forbidden().json(
-            crate::http::HttpError {
-                error: format!("You do not have access to file {filename}"),
-            }
-        ),
+    if !claims.data.file_access_authorized(&filename) {
+        return HttpResponse::Forbidden()
+            .json(HttpError {
+                error: format!(
+                    "You do not have access to file {filename}",
+                ),
+            });
     }
+    let filename = if filename.starts_with("/var/log") {
+        filename.to_owned()
+    } else {
+        format!("/var/log/{filename}")
+    };
+
+    if !Path::new(&filename).is_file() {
+        return HttpResponse::BadRequest()
+            .json(HttpError {
+                error: format!("{filename} does not exist."),
+            });
+    }
+
+    let pat = logs_req.pattern
+        .to_owned()
+        .unwrap_or(String::new());
+
+    let mut rev_log_reader = {
+        match RevLogReader::new(filename) {
+            Ok(r) => r,
+            Err(e) => {
+                error!("Error reading from log file: {e}");
+                return internal_err();
+            },
+        }
+    };
+    let lines = rev_log_reader
+        .iter()
+        .filter(|line| line.contains(pat.as_str()))
+        .skip(logs_req.skip.unwrap_or(0))
+        .take(logs_req.take.unwrap_or(10))
+        .collect::<Vec<_>>();
+
+    HttpResponse::Ok().json(
+        lines,
+    )
 }
